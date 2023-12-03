a16a67a87f40fa648ee6808bf2f315c562dc8efd
Francis Deh <dfdemajesty699@gmail.com>
2023-12-03 23:57:38 +0100

    chore: update file upload logic

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,28 @@
-const express = require('express');
+const express = require('express')
 const multer = require("multer")
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/images')
+    cb(null, 'public/uploads')
   },
   filename: function (req, file, cb) {
-    let ext = file.originalname.split(".")[1]
-    cb(null, Date.now() + "." + ext)
+    let ext = file.mimetype.split("/")[1]
+    cb(null, `${file.fieldname}-${Date.now()}.${ext}`)
   }
 })
 
-const file_upload = multer({ storage: storage })
+const file_upload = multer({ storage: storage, limits: { fileSize: 1000000}, fileFilter: function(req, file, cb) {
+  let allowed_mime_types = [
+    "image/png",
+    "image/jpeg",
+    "image/jpeg",
+  ]
+  if(!allowed_mime_types.includes(file.mimetype)) {
+     cb(new Error(`File is invalid. The file you uploaded is ${file.mimetype}.`))
+  }  
+},  
+})
+
 
 const router = express.Router();
 
@@ -21,9 +32,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Books-Gram' });
 });
 
-router.post("/book-cover-image-upload", file_upload.single("book-cover-image"), (req, res) => {
-  console.log(req.file)
-  return res.render("image", { title: "Image Viewer", image_url: req.file.filename })
+router.post("/book-cover-image-upload", file_upload.single("book-cover-image"), (req, res, next) => {
+  return res.render("image", { title: "Upload Viewer", image_url: req.file.filename })
 });
 
 module.exports = router;
